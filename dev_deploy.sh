817b8b3638e740282f2ff20a951f3cfb7306dbea
fangtao <fangtao@minijoy.work>
2023-09-01 14:02:44 +0800

    docs: 文案

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -9,5 +9,5 @@ yarn \
     && rsync --timeout=60 -avzP ${SRC}/* ${DEST} \
     && echo "Upload success" \
     && curl -X POST -H "Content-Type: application/json" \
-        -d '{"msg_type":"post","content":{"post":{"zh_cn":{"title":"Combo Campaign 测试环境","content":[[{"tag":"text","text":"项目有更新: "},{"tag":"a","text":"请查看","href":"https://opbnb-mainnet-debug.particle.network/"}]]}}}}' \
+        -d '{"msg_type":"post","content":{"post":{"zh_cn":{"title":"opBNB Mainnet Campaign 测试环境","content":[[{"tag":"text","text":"项目有更新: "},{"tag":"a","text":"请查看","href":"https://opbnb-mainnet-debug.particle.network/"}]]}}}}' \
         https://open.feishu.cn/open-apis/bot/v2/hook/72300a7e-d346-43d6-a796-a374ad4977b1
