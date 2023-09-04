a25d4c2131118f8bfc162fa28955b4f533a439ca
fangtao <fangtao@minijoy.work>
2023-09-04 20:42:51 +0800

    ci: 上线脚本

--- dev_deploy.sh
+++ dev_deploy.sh
@@ -14,5 +14,5 @@ yarn \
     && rsync --timeout=60 -avzP ${SRC}/* ${DEST3} \
     && echo "Upload success" \
     && curl -X POST -H "Content-Type: application/json" \
-        -d '{"msg_type":"post","content":{"post":{"zh_cn":{"title":"Campaign 测试环境","content":[[{"tag":"a","text":"Combo Testnet","href":"https://combo-testnet-debug.particle.network/"},{"tag":"text","text":" | "},{"tag":"a","text":"Scroll Sepolia","href":"https://scroll-testnet-debug.particle.network/"},{"tag":"text","text":" | "},{"tag":"a","text":"opBNB","href":"https://opbnb-mainnet-debug.particle.network/"}]]}}}}' \
+        -d '{"msg_type":"post","content":{"post":{"zh_cn":{"title":"Campaigns 测试环境","content":[[{"tag":"a","text":"Combo Testnet","href":"https://combo-testnet-debug.particle.network/"},{"tag":"text","text":" | "},{"tag":"a","text":"Scroll Sepolia","href":"https://scroll-testnet-debug.particle.network/"},{"tag":"text","text":" | "},{"tag":"a","text":"opBNB","href":"https://opbnb-mainnet-debug.particle.network/"}]]}}}}' \
         https://open.feishu.cn/open-apis/bot/v2/hook/72300a7e-d346-43d6-a796-a374ad4977b1
