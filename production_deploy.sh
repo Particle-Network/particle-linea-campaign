6e934770f0aa67f1b17eacedbcae939611eb1761
Felix <chuanfa.chai@gmail.com>
2023-09-08 13:42:25 +0800

    ci: fix publish

--- production_deploy.sh
+++ production_deploy.sh
@@ -25,8 +25,8 @@ yarn install &&
     rm ${sourceDir}/index.html.backup.${timestamp} &&
     cp ${sourceDir}/index.html ${sourceDir}/index.html.backup.${timestamp} &&
     ossutilmac64 cp -r -f --include "index.html*" ${sourceDir}/ oss://${bucketName3}/ &&
-    echo "\033[32m \nUpload success\n \033[0m" \
-        curl -X POST -H "Content-Type: application/json" \
+    echo "\033[32m \nUpload success\n \033[0m" &&
+    curl -X POST -H "Content-Type: application/json" \
         -d '{"msg_type":"post","content":{"post":{"zh_cn":{"title":"Campaigns 正式环境","content":[[{"tag":"a","text":"Combo Testnet","href":"https://combo-testnet.particle.network/"},{"tag":"text","text":" | "},{"tag":"a","text":"Scroll Sepolia","href":"https://scroll-testnet.particle.network/"},{"tag":"text","text":" | "},{"tag":"a","text":"opBNB","href":"https://opbnb-mainnet.particle.network/"}]]}}}}' \
         https://open.feishu.cn/open-apis/bot/v2/hook/72300a7e-d346-43d6-a796-a374ad4977b1
 
